34c5351fca4866abd6989b465acc56a70abda690
MahalaxmiSadula <sadulamahalaxmi9@gmail.com>
2024-02-23 21:11:43 -0800

    @EnableCaching added

--- src/main/java/com/microservice/accountsservice/AccountsServiceApplication.java
+++ src/main/java/com/microservice/accountsservice/AccountsServiceApplication.java
@@ -2,8 +2,10 @@ package com.microservice.accountsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class AccountsServiceApplication {
 
 	public static void main(String[] args) {
